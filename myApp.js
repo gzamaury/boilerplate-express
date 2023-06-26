b608ed6c0c16e3f1ab05a1f49b95e87cf240f5b9
Replit user <>
2023-06-26 23:37:53 +0000

    chore: Mount the express.static() middleware to the path /public with app.use()

--- myApp.js
+++ myApp.js
@@ -1,12 +1,13 @@
 let express = require('express');
 let app = express();
 const indexPath = __dirname + '/views/index.html'
+const assetsPath = __dirname + '/public'
+const middAssets = express.static(assetsPath)
 
 console.log("Hello World")
 
-
-app.get("/", (req, res) => res.sendFile(indexPath))
-
+app.get('/', (req, res) => res.sendFile(indexPath))
+app.use('/public', middAssets)
 
 
 
