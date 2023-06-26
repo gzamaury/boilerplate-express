c7e58fd3e5e2702bd03fd9d931be22442d894ae9
Replit user <>
2023-06-26 23:25:37 +0000

    chore: Send the /views/index.html file as a response to GET requests to the / path

--- myApp.js
+++ myApp.js
@@ -1,13 +1,11 @@
 let express = require('express');
 let app = express();
+const indexPath = __dirname + '/views/index.html'
 
 console.log("Hello World")
 
-app.get("/", (req, res) => res.send("Hello Express"))
 
-
-
-
+app.get("/", (req, res) => res.sendFile(indexPath))
 
 
 
